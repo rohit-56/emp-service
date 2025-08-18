e86e344c20441083b6ddb910be8e0600fe254a7f
rohit-56 <rohitmou25@gmail.com>
2025-08-18 21:57:01 +0530

    add-kafka

--- src/main/java/com/rslearning/employee/config/KafkaConfig.java
+++ src/main/java/com/rslearning/employee/config/KafkaConfig.java
@@ -1,5 +1,18 @@
 package com.rslearning.employee.config;
 
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.TopicBuilder;
+
+
+@EnableKafka
 public class KafkaConfig {
+
+    @Bean
+    public NewTopic createEmployeeTopic(){
+        return TopicBuilder.name("emp-service-topic").build();
+    }
+
 }
